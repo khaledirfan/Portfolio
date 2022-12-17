92d1b9cd566a00dfd28bfea03f4deb964e53dab9
Khaled Hasan Irfan <khaledirfan7@gmail.com>
2022-12-18 01:51:11 +0600

    second update

--- script.js
+++ script.js
@@ -1,20 +1,16 @@
-$(document).ready(function(){
-    $(window).scroll(function(){
-       if (this.scrollY > 20){
-          $('.navbar').addClass("sticky");
-        }
-       else{
-          $('.navbar').removeClass("sticky");
-        }
-       if (this.scrollY > 500){
-          $('.scroll-up-btn').addClass("show");
-        }
-       else{
-          $('.scroll-up-btn').removeClass("show");
-         }
+$(document).ready(function () {
+  if (this.scrollY > 20) {
+    $(".navbar").addClass("sticky");
+  } else {
+    $(".navbar").removeClass("sticky");
+  }
+  if (this.scrollY > 500) {
+    $(".scroll-up-btn").addClass("show");
+  } else {
+    $(".scroll-up-btn").removeClass("show");
+  }
 });
 
-//slide-up script
-$('.scroll-up-btn').click(function(){
-  $('html').animate({screenTop: 0});
+$(".scroll-up-btn").click(function () {
+  $("html").animate({ screenTop: 0 });
 });
